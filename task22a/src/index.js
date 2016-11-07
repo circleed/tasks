0972ce3c4edf2468c4e19998da2f320c816e81de
Daniil <frlns@mail.ru>
2016-11-07 14:16:31 +0300

    task22a

--- task22a/src/index.js
+++ task22a/src/index.js
@@ -4,6 +4,12 @@
 import express from 'express';
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => {
 
     const a = +req.query.a;

8ee947c944d81744db102442ba2bc32bcaaca03b
Daniil <frlns@mail.ru>
2016-11-12 10:33:36 +0300

    task3c

--- task3a/src/index.js
+++ task3a/src/index.js
@@ -1,15 +1,12 @@
 /**
  * Created by Daniil on 12.11.2016.
  */
-import mongoose from 'mongoose';
-import Promise from 'bluebird';
+
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import fetch from 'isomorphic-fetch';
 import _ from "lodash";
-mongoose.promise = Promise;
-mongoose.connect('mongodb://publicdb.mgbeta.ru/dd_skb3');
 
 const app = express();
 app.use(cors());
@@ -85,50 +82,29 @@ app.get('/:prop1/:prop2', (req, res) => {
     if(!pc[req.params.prop1] || !pc[req.params.prop1][req.params.prop2]) {
         return res.status(404).send('Not found');
     }
-    if(req.params.prop1 == 'floppy') {
-        return res.json(pc.floppy[req.params.prop2]);
-    }
-    if(req.params.prop1 == 'board') {
-        return res.json(pc.board[req.params.prop2]);
-    }
-    if(req.params.prop1 == 'os') {
-        return res.json(pc.os[req.params.prop2]);
-    }
-    if(req.params.prop1 == 'hdd' && pc.hdd[req.params.prop2]) {
-        return res.json(pc.hdd[req.params.prop2]);
-    }
-    if(req.params.prop1 == 'monitor') {
-        return res.json(pc.monitor[req.params.prop2]);
-    }
-    if(req.params.prop1 == 'ram') {
-        return res.json(pc.ram[req.params.prop2]);
+
+    switch(req.params.prop1) {
+        case 'floppy': {
+            return res.json(pc.floppy[req.params.prop2]);
+        }
+        case 'board': {
+            return res.json(pc.board[req.params.prop2]);
+        }
+        case 'os': {
+            return res.json(pc.os[req.params.prop2]);
+        }
+        case 'hdd': {
+            return res.json(pc.hdd[req.params.prop2]);
+        }
+        case 'monitor': {
+            return res.json(pc.monitor[req.params.prop2]);
+        }
+        case 'ram': {
+            return res.json(pc.ram[req.params.prop2]);
+        }
     }
     return res.status(404).send('Not found');
 });
-
-// app.get('/ram', async (req, res) => {
-//     return res.json(pc.ram);
-// });
-//
-// app.get('/os', (req, res) => {
-//     return res.json(pc.os);
-// });
-//
-// app.get('/floppy', (req, res) => {
-//     return res.json(pc.floppy);
-// });
-//
-// app.get('/hdd', (req, res) => {
-//     return res.json(pc.hdd);
-// });
-//
-// app.get('/monitor', (req, res) => {
-//     return res.json(pc.monitor);
-// });
-//
-// app.get('/board', (req, res) => {
-//     return res.json(pc.board);
-// });
 
 app.get('/floppy/:prop', (req, res) => {
     if(req.params.prop) {
@@ -175,21 +151,6 @@ app.get('/ram/:prop', (req, res) => {
         return res.status(404).send('Not found');
     }
 });
-
-// app.get('/volumes', (req, res) => {
-//
-//     const sum = _(pc.hdd)
-//         .groupBy('volume')
-//         .map((v, k) => ({
-//             volume: k,
-//             size:  _.sumBy(v, 'size')
-//         })).value();
-//     return res.json(    {
-//         "C:" : `${sum[0].size}B`,
-//         "D:" : `${sum[1].size}B`
-//     });
-//
-// });
 
 app.get('/hdd/:propNumber/:propName', (req, res) => {
     if(pc.hdd[+req.params.propNumber] && req.params.propNumber && req.params.propName) {
